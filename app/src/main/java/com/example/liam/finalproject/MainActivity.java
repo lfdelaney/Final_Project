cd56ec6965b1231a2995514e8756aa7b6cf268b9
lfdelaney <lfdelane@syr.edu>
2016-04-25 17:04:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/liam/finalproject/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.liam.finalproject;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import java.lang.reflect.Type;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView title = (TextView) findViewById(R.id.title);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/playball.ttf");
+        title.setTypeface(tf);
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
