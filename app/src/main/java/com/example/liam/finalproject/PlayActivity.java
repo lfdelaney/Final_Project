bcca307d3117202bbe22b8ac79e0d3499450c4ea
electricfr34k <rdpierce@syr.edu>
2016-04-28 00:56:32 -0400

    anotha one

--- app/src/main/java/com/example/liam/finalproject/PlayActivity.java
+++ app/src/main/java/com/example/liam/finalproject/PlayActivity.java
@@ -1,5 +1,6 @@
 package com.example.liam.finalproject;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,16 +8,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
 
 public class PlayActivity extends AppCompatActivity {
 
+    int count = 0;
+    int flag = -2;
     Spinner homeSpinner;
     Spinner awaySpinner;
-    String[] nameList = new String[]{};
+    String[] nameList;
     ArrayAdapter<String> adapter;
-    TeamData team = new TeamData();
-    String temp;
+    Firebase ref = new Firebase("https://diamond-tracker.firebaseio.com/League");
 
 
     @Override
@@ -24,36 +34,76 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        //homeSpinner = (Spinner)findViewById(R.id.homeTeam);
-        //awaySpinner = (Spinner)findViewById(R.id.awayTeam);
+        TextView title = (TextView) findViewById(R.id.name);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/playball.ttf");
+        title.setTypeface(tf);
 
+         nameList = new String[100];
 
-        for(int i = 0; i<= team.getSize(); i++){
-            temp = (String)team.getItem(i).get("name");
-            nameList[i] = temp;
-            Log.d("Team List: ", temp);
+        homeSpinner = (Spinner)findViewById(R.id.homeTeam);
+        awaySpinner = (Spinner)findViewById(R.id.awayTeam);
+
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                HashMap<String, ?> team = (HashMap<String, ?>) dataSnapshot.getValue();
+                nameList[count] = (String) team.get("name");
+                Log.d("Added to list: ", nameList[count]);
+                count++;
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        if(flag == count) {
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, nameList);
+            homeSpinner.setAdapter(adapter);
+            awaySpinner.setAdapter(adapter);
+            homeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    Log.v("item", (String) parent.getItemAtPosition(position));
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // TODO Auto-generated method stub
+                }
+            });
+            awaySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    Log.v("item", (String) parent.getItemAtPosition(position));
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // TODO Auto-generated method stub
+                }
+            });
+        }else{
+            flag++;
+            Log.d("FLAG = ", "false");
         }
-
-
-
-        //adapter = new ArrayAdapter<String>(this,R.layout.spinner_layout, nameList);
-        homeSpinner.setAdapter(adapter);
-        awaySpinner.setAdapter(adapter);
-
-        homeSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Clicked Home Team Spinner Item", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        awaySpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Clicked Away Team Spinner Item", Toast.LENGTH_LONG).show();
-            }
-        });
-
-
     }
 }
